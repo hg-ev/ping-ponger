10941cfdd3d586ca45a5a26d17b8cf598ab0ef32
HG King <hgking@Admins-MacBook-Pro.local>
2019-10-02 13:18:38 -0400

    trying to add a database to this app

--- api/app.js
+++ api/app.js
@@ -3,8 +3,8 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.API_PORT_D
-const ping = process.env.API_PING_D
+const port = process.env.WEB_PORT_D
+const ping = process.env.WEB_PING_D
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/ping', (req, res) => res.send( ping ))
